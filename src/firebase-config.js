6854d70da043281b51e64ab5861700990323c9c4
mgulaydr <114770330+mgulaydr@users.noreply.github.com>
2026-06-13 21:25:13 +0300

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,12 +1,12 @@
 // Firebase Console > Project settings > Web app config içeriğini buraya girin.
 // Bu dosya placeholder bırakılırsa uygulama demo/localStorage modunda çalışır.
 export const firebaseConfig = {
-  apiKey: 'REPLACE_WITH_FIREBASE_API_KEY',
-  authDomain: 'REPLACE_WITH_PROJECT_ID.firebaseapp.com',
-  projectId: 'REPLACE_WITH_PROJECT_ID',
-  storageBucket: 'REPLACE_WITH_PROJECT_ID.appspot.com',
-  messagingSenderId: 'REPLACE_WITH_MESSAGING_SENDER_ID',
-  appId: 'REPLACE_WITH_APP_ID'
+  apiKey: "AIzaSyA0TvPMyOrV00pc7uvQTO6YAj6Dftq5l3c",
+  authDomain: "esti-biraz-63227.firebaseapp.com",
+  projectId: "esti-biraz-63227",
+  storageBucket: "esti-biraz-63227.firebasestorage.app",
+  messagingSenderId: "262039594835",
+  appId: "1:262039594835:web:d5b29e26977c7764d8e3a6"
 };
 
 export const adminEmails = ['mehmet.gulay@ogr.iu.edu.tr'];
